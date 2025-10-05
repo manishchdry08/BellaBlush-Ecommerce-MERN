aebb88c87826be465c62bd675c2daa21402113d3
your_github_manishchdry08 <manishchdry08@gmail.com>
2025-10-05 21:13:25 +0530

    Remove old backend folder

--- glowcart-backend/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(requiredRole) {
-  return (req, res, next) => {
-    if (!req.user || req.user.role !== requiredRole) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-    next();
-  };
-};
